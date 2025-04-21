c41c8c83d1593089b361c62990c1bc6ee443db44
Petr Skoda <petr@mutms.org>
2025-04-21 13:04:34 +0200

    bump version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'customfield_mutrain';
-$plugin->version   = 2025041501;
+$plugin->version   = 2025042100;
 $plugin->requires  = 2024091700.00; // 4.5.0
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
